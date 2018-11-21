1d93c58c03c573a0f1d49b069f8f491da780bc4f
thanhhaidev <hai2571998@gmail.com>
2018-11-21 10:38:01 +0700

    Demo Commit Visual Code CS

---
+++ QTStore/QTStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QTStore.Startup))]
+namespace QTStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
